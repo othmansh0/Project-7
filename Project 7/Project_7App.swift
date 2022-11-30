e7bab5efabaca184d6acd0aa76b09ac4b3df67d0
othman shahrouri <othmanshahrouri@gmail.com>
2022-11-30 20:24:37 +0300

    Initial Commit

---
+++ Project 7/Project_7App.swift
@@ -0,0 +1,17 @@
+//
+//  Project_7App.swift
+//  Project 7
+//
+//  Created by Rana Shahrouri on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
